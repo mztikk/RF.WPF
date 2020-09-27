fe0d831773614397985eaed7fee95dca55ef9ba0
mztikk <nevigskcuforez@gmail.com>
2020-09-27 08:41:50 +0200

    added stringlength visibility converter

---
+++ RF.WPF/MVVM/Converters/StringLengthVisibleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class StringLengthVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Length > 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else if (value is null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+}
