dfba19727b7ed58a67fb0807fc385feadc532464
mztikk <nevigskcuforez@gmail.com>
2020-09-27 13:14:07 +0200

    added iocbase, refactored storage di out

---
+++ RF.WPF/IocBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using RFReborn.Internals;
+using StyletIoC;
+
+namespace RF.WPF
+{
+    public abstract class IocBase : StyletIoCModule
+    {
+        protected readonly IEnumerable<Assembly> _assemblies;
+
+        public IocBase(IEnumerable<Assembly> assemblies) => _assemblies = assemblies;
+
+        public IocBase() : this(new HashSet<Assembly> { typeof(IocSetup).Assembly, Assembly.GetExecutingAssembly(), Assembly.GetEntryAssembly(), Assembly.GetCallingAssembly() }) { }
+        protected IEnumerable<Type> GetAllTypes() => AssemblyInfo.GetAllTypes(_assemblies);
+
+        public virtual void Configure(IContainer container) { }
+    }
+}
--- RF.WPF/IocSetup.cs
+++ RF.WPF/IocSetup.cs
@@ -1,59 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
 using RF.WPF.MVVM;
 using RF.WPF.Navigation;
-using RFReborn.Internals;
 using RFReborn.RandomR;
-using StyletIoC;
 
 namespace RF.WPF
 {
-    public sealed class IocSetup : StyletIoCModule
+    public sealed class IocSetup : IocBase
     {
         private static readonly string[] s_viewSuffix = new string[] { "View" };
         private static readonly string[] s_viewModelSuffix = new string[] { "ViewModel", "VM" };
 
-        private readonly IEnumerable<Assembly> _assemblies;
-
-        private readonly List<Type> _storageTypes = new List<Type>();
-
-        public IocSetup(IEnumerable<Assembly> assemblies) => _assemblies = assemblies;
-
-        public IocSetup() : this(new HashSet<Assembly> { typeof(IocSetup).Assembly, Assembly.GetExecutingAssembly(), Assembly.GetEntryAssembly(), Assembly.GetCallingAssembly() }) { }
-
         protected override void Load()
         {
-            Bind<IocSetup>().ToInstance(this);
+            Bind<IocBase>().ToInstance(this);
             Bind<Random>().To<CryptoRandom>().InSingletonScope();
             Bind<INavigationService>().To<NavigationService>().InSingletonScope();
 
-            SetupStorage();
             SetupMVVM();
-        }
-
-        public void Configure(IContainer container)
-        {
-            foreach (Type type in _storageTypes)
-            {
-                var storage = container.Get(type) as IStorage;
-                storage?.ILoad();
-            }
-        }
-
-        private IEnumerable<Type> GetAllTypes() => AssemblyInfo.GetAllTypes(_assemblies);
-
-        private void SetupStorage()
-        {
-            foreach (Type type in GetAllTypes())
-            {
-                string? baseName = type.BaseType?.FullName;
-                if (baseName?.StartsWith("RF.WPF.Storage`1") == true)
-                {
-                    Bind(type).ToSelf().InSingletonScope();
-                    _storageTypes.Add(type);
-                }
-            }
         }
 
         private void SetupMVVM()
