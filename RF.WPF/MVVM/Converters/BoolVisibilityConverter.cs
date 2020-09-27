72a72c4f8b03d9e8cd63b52367ad84a3ce4fef7a
mztikk <nevigskcuforez@gmail.com>
2020-09-27 09:23:07 +0200

    added bool visiblity converter

---
+++ RF.WPF/MVVM/Converters/BoolVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else if (value is null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+            {
+                return v switch
+                {
+                    Visibility.Visible => true,
+                    Visibility.Hidden => false,
+                    Visibility.Collapsed => false,
+                    _ => false,
+                };
+            }
+            else if (value is null)
+            {
+                return false;
+            }
+
+            throw new ArgumentException();
+        }
+    }
+
+}
