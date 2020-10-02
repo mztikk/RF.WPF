99119841869361c1ec765287e8c21840e450038c
mztikk <nevigskcuforez@gmail.com>
2020-10-02 18:28:36 +0200

    added genericroutine

---
+++ RF.WPF/GenericRoutine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using RFReborn.Routines;
+
+namespace RF.WPF
+{
+    public class GenericRoutine : RoutineBase
+    {
+        private readonly Func<Task> _func;
+
+        public GenericRoutine(TimeSpan cooldownTime, Func<Task> func) : base(cooldownTime) => _func = func;
+
+        protected override async Task OnTick() => await _func?.Invoke();
+
+        protected override Task Shutdown() => throw new NotImplementedException();
+    }
+}
