c4cf06bdf1835e5d95d7e70bd2896071d77ddc3c
mztikk <nevigskcuforez@gmail.com>
2020-10-03 15:39:16 +0200

    added startup class

---
+++ RF.WPF/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace RF.WPF
+{
+    public static class Startup
+    {
+        public static void AddToStartup()
+        {
+            Microsoft.Win32.RegistryKey? key = null;
+            try
+            {
+                key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                Assembly curAssembly = Assembly.GetEntryAssembly();
+                string name = curAssembly.GetName().Name;
+                key.SetValue(name, curAssembly.Location);
+            }
+            finally
+            {
+                key?.Close();
+                key?.Dispose();
+            }
+        }
+
+        public static void RemoveFromStartup()
+        {
+            Microsoft.Win32.RegistryKey? key = null;
+            try
+            {
+                key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                Assembly curAssembly = Assembly.GetEntryAssembly();
+                string name = curAssembly.GetName().Name;
+                if (key.GetValue(name) is { })
+                {
+                    key.DeleteValue(name);
+                }
+            }
+            finally
+            {
+                key?.Close();
+                key?.Dispose();
+            }
+        }
+    }
+}
