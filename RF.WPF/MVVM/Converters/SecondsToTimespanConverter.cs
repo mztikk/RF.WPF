65b86262ad2e89415010874db8a2155428aa081c
mztikk <nevigskcuforez@gmail.com>
2020-10-03 15:39:05 +0200

    added secondstotimespanconverter

---
+++ RF.WPF/MVVM/Converters/SecondsToTimespanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class SecondsToTimespanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timespan)
+            {
+                return timespan.TotalSeconds;
+            }
+
+            throw new ArgumentException(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && double.TryParse(str, out double seconds))
+            {
+                return TimeSpan.FromSeconds(seconds);
+            }
+
+            throw new ArgumentException(value?.ToString());
+        }
+    }
+
+}
