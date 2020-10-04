f1f2e8b08f7133aa25b0310b8f7dbecf8e52d245
mztikk <nevigskcuforez@gmail.com>
2020-10-04 11:08:52 +0200

    added DateTimeToLocalConverter

---
+++ RF.WPF/MVVM/Converters/DateTimeToLocalConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateTimeToLocalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToLocalTime().ToString();
+            }
+
+            throw new ArgumentException(value.ToString());
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+
+
+    }
+}
