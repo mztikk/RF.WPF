8b4c1690df208257668fb9532c3d46261f55b937
mztikk <nevigskcuforez@gmail.com>
2020-10-24 11:36:30 +0200

    added InverseBoolVisibilityConverter

---
+++ RF.WPF/MVVM/Converters/InverseBoolVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class InverseBoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is null)
+            {
+                return Visibility.Visible;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+            {
+                return v switch
+                {
+                    Visibility.Visible => false,
+                    Visibility.Hidden => true,
+                    Visibility.Collapsed => true,
+                    _ => true,
+                };
+            }
+            else if (value is null)
+            {
+                return true;
+            }
+
+            throw new ArgumentException();
+        }
+    }
+
+}
