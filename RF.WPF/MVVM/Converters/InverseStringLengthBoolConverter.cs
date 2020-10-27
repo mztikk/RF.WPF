75f442e113d0664a37fd4137495f4b0844d8d1e8
mztikk <nevigskcuforez@gmail.com>
2020-10-27 18:29:25 +0100

    added StringLengthBoolConverter and InverseStringLengthBoolConverter

---
+++ RF.WPF/MVVM/Converters/InverseStringLengthBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class InverseStringLengthBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Length > 0 ? false : true;
+            }
+            else if (value is null)
+            {
+                return true;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+}
---
+++ RF.WPF/MVVM/Converters/StringLengthBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RF.WPF.MVVM.Converters
+{
+    public class StringLengthBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Length > 0 ? true : false;
+            }
+            else if (value is null)
+            {
+                return false;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+}
