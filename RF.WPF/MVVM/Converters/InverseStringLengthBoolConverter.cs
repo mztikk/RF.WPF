773d6643894d39eb6057ffb39a22a193bf514a59
mztikk <nevigskcuforez@gmail.com>
2020-10-27 18:31:30 +0100

    code cleanup

--- RF.WPF/MVVM/Converters/InverseStringLengthBoolConverter.cs
+++ RF.WPF/MVVM/Converters/InverseStringLengthBoolConverter.cs
@@ -6,19 +6,12 @@ namespace RF.WPF.MVVM.Converters
 {
     public class InverseStringLengthBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value switch
         {
-            if (value is string str)
-            {
-                return str.Length > 0 ? false : true;
-            }
-            else if (value is null)
-            {
-                return true;
-            }
-
-            throw new ArgumentException();
-        }
+            string str => str.Length == 0,
+            null => true,
+            _ => throw new ArgumentException(),
+        };
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
--- RF.WPF/MVVM/Converters/StringLengthBoolConverter.cs
+++ RF.WPF/MVVM/Converters/StringLengthBoolConverter.cs
@@ -6,19 +6,12 @@ namespace RF.WPF.MVVM.Converters
 {
     public class StringLengthBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value switch
         {
-            if (value is string str)
-            {
-                return str.Length > 0 ? true : false;
-            }
-            else if (value is null)
-            {
-                return false;
-            }
-
-            throw new ArgumentException();
-        }
+            string str => str.Length > 0,
+            null => false,
+            _ => throw new ArgumentException(),
+        };
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
